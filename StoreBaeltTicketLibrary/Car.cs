7172ef12ff917d794b0deb2a3efacbf678fd734d
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2022-10-04 11:45:37 +0200

    Created a car override in storebealtLib

---
+++ StoreBaeltTicketLibrary/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace StoreBaeltTicketLibrary
+{
+    public record Car(string Licensplate, DateTime Date, bool Brobizz = false)
+        : TicketSystemClassLibrary.Car(Licensplate, Date, Brobizz)
+    {
+        public override double Price()
+        {
+            double price = StartPrice;
+            const double weekendDiscount = 0.2;
+
+            if (Date.DayOfWeek == DayOfWeek.Sunday || Date.DayOfWeek == DayOfWeek.Saturday)
+                price *= 1 - weekendDiscount;
+
+            if (Brobizz)
+                price *= 1 - BrobizzDiscountPrecent;
+
+            return price;
+        }
+    }
+}
---
+++ TicketSystemClassLibTest/StorebealtCar.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibTest
+{
+    [TestClass]
+    public class StorebealtCar
+    {
+        [TestMethod]
+        [DataRow("2022/10/3 10:00:00", false, 240)] //monday
+        [DataRow("2022/10/3 10:00:00", true, 228)] //monday
+        [DataRow("2022/10/2 10:00:00", false, 192)] //sunday
+        [DataRow("2022/10/2 10:00:00", true, 182.4)] //monday
+        public void CarPrice(string date, bool brobizz, double expected)
+        {
+            //arrange
+            Car car = new("9999999", DateTime.Parse(date), brobizz);
+
+            //assert
+            Assert.AreEqual(expected, car.Price(), 0.00000001);
+        }
+    }
+}

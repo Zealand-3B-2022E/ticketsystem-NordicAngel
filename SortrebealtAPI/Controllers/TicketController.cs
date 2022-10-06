229730d7698a3a4dc3cfaf8e2505bed85700641a
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2022-10-06 13:32:45 +0200

    Abit more to webApi

--- SortrebealtAPI/Controllers/TicketController.cs
+++ SortrebealtAPI/Controllers/TicketController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StoreBaeltTicketLibrary;
+using TicketSystemClassLibrary;
+using Car = StoreBaeltTicketLibrary.Car;
 using maneger = SortrebealtAPI.Managers.TicketManeger;
 
 namespace SortrebealtAPI.Controllers
@@ -9,6 +11,7 @@ namespace SortrebealtAPI.Controllers
     public class TicketController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult Get()
         {
             return Ok(maneger.Get());
@@ -16,6 +19,8 @@ namespace SortrebealtAPI.Controllers
 
         [HttpGet]
         [Route("{licensplate}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Get(string licensplate)
         {
             try
@@ -31,10 +36,20 @@ namespace SortrebealtAPI.Controllers
 
         [HttpPost]
         [Route("car")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public IActionResult CreateCar([FromBody] Car car)
         {
             maneger.Create(car);
             return Created("api/tickets" + car.Licensplate, car);
+        }
+
+        [HttpPost]
+        [Route("mc")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult CreateCar([FromBody] MC mc)
+        {
+            maneger.Create(mc);
+            return Created("api/tickets" + mc.Licensplate, mc);
         }
     }
 }
